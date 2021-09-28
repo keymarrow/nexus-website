90f137ef1caf8a4782a784370f699069324011c1
Joan Henry <jhenry081@gmail.com>
2021-09-28 09:17:02 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# REPO TEMPLATE FOR WEBSITES
+
+This is a repo for static sites with the following contents:
+
+* Project structure/dictories
+
+* Navigation (logo and nav links)
+
+* Responsive navigation
+
+* Banner
+
+* Form styles
+
+* Flexbox utilities
+
+* README
+
+* Footer
---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Accordion Functionality
+document.querySelectorAll(".accordion").forEach(acc => {
+  acc.addEventListener("click", function() {
+    acc.classList.toggle("open");
+  });
+});
+
+// Automatically setting the date in footer
+const date = new Date();
+const datePlaceholder = document.getElementById('date').innerHTML = date.getFullYear();
+console.log(datePlaceholder);
+
+// Navigation menu toggle 
+
+function myFunction() {
+  var x = document.querySelector("#test");
+  x.classList.toggle('change');
+  if (x.style.display === "block") {
+   x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
